cf3237a9a342039857b7380a6e8e81a5884d5b63
Mariam-Abugamga <71400493+Mariam-Abugamga@users.noreply.github.com>
2020-09-28 14:04:29 -0400

    added a comment

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 let current_temperature = 0
+// Created By: Mariam Abugamga
+// Created on: sept 28, 2020
+// This program uses a temperature
 basic.forever(function () {
     current_temperature = input.temperature()
     basic.showNumber(current_temperature)
